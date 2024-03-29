f1ebcaabbe59dff8521adb60639ef33176985471
Terapico <89236406+younghwalkim@users.noreply.github.com>
2024-03-29 20:21:24 +0900

    Delete getdrive/dbscript directory

--- getdrive/dbscript/user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER c##testproject IDENTIFIED BY testproject;
-GRANT CONNECT, RESOURCE TO c##testproject;
-GRANT CREATE VIEW TO c##testproject;
-ALTER USER c##testproject
-QUOTA 1024M ON USERS;
